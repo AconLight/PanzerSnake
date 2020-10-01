7492bab132bf97a662de50820cb662563d65cbf2
Wiktor <wiktor.muraszko@wp.pl>
2020-10-01 18:39:25 +0200

    Meni

---
+++ PanzerSnakePro/Assets/scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    int licznikBoluDupy = 0;
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void DUPA()
+    {
+        licznikBoluDupy += 1;
+    }
+
+}
