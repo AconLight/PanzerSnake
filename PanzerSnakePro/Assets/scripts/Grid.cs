8f9f10e2d397a6bcc791f49d908d579fd72d98d0
AconLight <w.mosienski@gmail.com>
2020-10-01 13:01:14 +0200

    bg

---
+++ PanzerSnakePro/Assets/scripts/Grid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+
+    public GameObject gridElementPrefab; 
+    public int sizeX;
+    public int sizeY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = -(sizeX-1)/2; i <= (sizeX-1)/2; i++) {
+            for (int j = -(sizeY-1)/2; j <= (sizeY-1)/2; j++) {
+                Instantiate(gridElementPrefab, new Vector3(i*2, j*2, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
